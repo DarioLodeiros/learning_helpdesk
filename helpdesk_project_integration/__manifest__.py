f5004c6de6358de18686910a2180e50bee0c60f9
imoralesc <ignacio.morales@factorlibre.com>
2020-07-29 16:28:13 +0200

    ADD project_integration Tests

---
+++ helpdesk_project_integration/__manifest__.py
@@ -0,0 +1,13 @@
+ 
+{
+    'name': 'Helpdesk Integration with Project',
+    'version': '11.0',
+    'author': 'Nacho Morales',
+    'license': 'AGPL-3',
+    'description': """
+        Module to integrate Project inside Helpdesk.
+    """,
+    'depends': [],
+    'data': [],
+    'installable': True
+}
---
+++ helpdesk_project_integration/tests/test_integration_project_helpdesk.py
@@ -0,0 +1,22 @@
+# Test ticket associated to a project
+'''
+def count_ticket_in_project(self):
+    -Create the new ticket
+    -assign the new ticket to a project
+    -button project_ticket_count +1
+    -check the count button is right
+'''
+
+#Test project_id on helpdesk_ticket
+'''
+    -Check project_ticket_id = new_ticket_project_id
+'''
+
+#Test when the project change, ticket reset
+'''
+    def test_helpdesk_ticket_project_task(self):
+        -Create method onchange
+        -Check project_id change
+        -If project_id change remove ticket_id 
+'''
+
