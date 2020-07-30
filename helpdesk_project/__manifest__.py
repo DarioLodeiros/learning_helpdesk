af7a25ff81f1ab4d46f3fce0bf5275abf6e45819
Alberto-Esteban <albertoestebanparra@gmail.com>
2020-07-30 10:16:14 +0200

    ADD helpdesk_project

---
+++ helpdesk_project/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2020 Nicolas Bessi (Camptocamp SA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Helpdesk Project',
+    'summary': "Split first name and last name for non company partners",
+    'version': '11.0.1.0.1',
+    'author': "Alberto Esteban",
+    'license': "AGPL-3",
+    'maintainer': 'Camptocamp, Acsone',
+    'category': 'Helpdesk',
+    'website': 'https://odoo-community.org/',
+    'depends': ['mail'],
+    'data': [
+        
+        ],
+    'installable': True,
+}
---
+++ helpdesk_project/tests/test_helpdesk_project.py
@@ -0,0 +1,15 @@
+#Ticket in a project
+'''
+def ticket_in_proyect(self):
+    #ticket belongs to a project
+    -we create the new ticket
+    -assign the new ticket to a project
+    -button ticket count +1
+    -check the count of the button is right
+'''
+#OnChange  
+'''
+def ticket_project_method(self):
+    -Create onchange
+    -Check onchange works
+'''
