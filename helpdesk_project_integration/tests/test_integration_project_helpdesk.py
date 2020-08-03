29bdf403b808ff2d9b16f8b28dd35ff273471ab3
imoralesc <ignacio.morales@factorlibre.com>
2020-08-03 07:58:32 +0200

    ADD Tests project_inegration

--- helpdesk_project_integration/tests/test_integration_project_helpdesk.py
+++ helpdesk_project_integration/tests/test_integration_project_helpdesk.py
@@ -20,3 +20,64 @@ def count_ticket_in_project(self):
         -If project_id change remove ticket_id 
 '''
 
+from odoo.tests import common
+
+@classmethod
+class TestHelpdeskTicketProject(common.SavepointCase):
+    def setUpClass(cls):
+        super(TestHelpdeskTicket, cls).setUpClass()
+        
+        Ticket = env['helpdesk.ticket']
+        Project = env["project.project"]
+        
+        cls.ticket1 = Ticket.create({
+            'name': 'Test 1',
+            'description': 'Ticket test1',
+        })
+        
+        cls.ticket2 = Ticket.create({
+            'name': 'Test 2',
+            'description': 'Ticket test2',
+        })
+        
+        cls.project1 = Project.create({
+            "name": "Test Helpdesk-Project 1",
+        })
+    
+        cls.project2 = Project.create({
+            "name": "Test Helpdesk-Project 2",
+        })
+      
+        cls.ticket1.write({
+            'project_id': cls.project1.id,
+        })
+        cls.ticket2.write({
+            'project_id': cls.project2.id,
+        })
+        
+    def test_helpdesk_ticket_project(self):
+        self.assertNotEquals(self.ticket.project, '/',
+                             'Helpdesk Ticket: A ticket should have '
+                             'a project.')
+        
+    def test_helpdesk_ticket_counts(self):
+        self.assertequal(self.project_id.ticket_count,
+                         1,
+                         'Helpdesk Ticket: Project assigned '
+                         'one ticket')
+        
+        self.assertequal(self.project_id.ticket_count,
+                         2,
+                         'Helpdesk Ticket: Project assigned '
+                         'two tickets')
+        
+        self.helpdesk_ticket1.write({
+            'stage_id': self.stage_closed.id,
+        })
+        
+        self.assertequal(self.project_id.ticket_count,
+                         1,
+                         'Helpdesk Ticket: Project assigned '
+                         'one ticket')
+
+
